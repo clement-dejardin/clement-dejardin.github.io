20e4bdedbf3b4b1f5840d0342d5db53f3ceec04a
Clément Dejardin <clement.dejardin@outlook.fr>
2019-10-07 19:38:10 +0200

    07/10/19

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-let departure
-let arrival
-function search() {
-    departure = document.getElementById('departure').value
-    arrival = document.getElementById('arrival').value
-    console.log(departure + ' ' + arrival)   
+let btn = document.getElementById('btn')
+let content = document.getElementById('content')
+let text = ""
+
+btn.addEventListener('click', function(){
+    var request = new XMLHttpRequest
+    request.open('GET', 'https://clement-dejardin.github.io/data.json')
+    request.onload = function() {
+        var data = JSON.parse(request.responseText)
+        renderHtml(data)
+    }
+    request.send()
+})
+
+function renderHtml(data) {
+    for (i = 1; i < data.length; i++) {
+        if (data[i].Departure == 'Annecy' || data[i].Arrival == 'Annecy' ) {
+            text += "<p> Departure: " + data[i].Departure + "<br> Arrival: " + data[i].Arrival + '<br> Price: ' + data[i].Single_pilot + data[i].Currency + '</p> <hr>'
+        }
+    }
+    content.insertAdjacentHTML('beforeend', text)
 }
-
-var request = new XMLHttpRequest
-request.open('GET', '')
-
-request.onload = function() {
-    console.log(request.responseText)
-}
-
-request.send()
