4b5dd4d907afc2338944d2e3a94c416fd0aeea60
Clément Dejardin <cdejardin@Clement.local>
2019-10-09 09:20:20 +0200

    Add seach

--- script.js
+++ script.js
@@ -2,15 +2,14 @@
 endpoint = 'https://clement-dejardin.github.io/data.json'
 const data = []
 fetch(endpoint)
-.then(blob => blob.json())
-.then(stuff => data.push(...stuff))
+    .then(blob => blob.json())
+    .then(stuff => data.push(...stuff))
 console.log(data)
 
-function renderHtml(data) {
-    for (i = 1; i < data.length; i++) {
-        if (data[i].Departure == 'Annecy' || data[i].Arrival == 'Annecy' ) {
-            text += "<p> Departure: " + data[i].Departure + "<br> Arrival: " + data[i].Arrival + '<br> Price: ' + data[i].Single_pilot + data[i].Currency + '</p> <hr>'
-        }
-    }
-    content.insertAdjacentHTML('beforeend', text)
+// Search in data
+function findMatches(wordToMatch, data) {
+    return data.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi')
+        return place.Departure.match(regex) || place.Arrival.match(regex)
+    })
 }
