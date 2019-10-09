cb9598f056c1ceb649d2abcf86d2c97ab38b5f5e
Clément Dejardin <cdejardin@Clement.local>
2019-10-09 13:48:29 +0200

    Search 2 city

--- script.js
+++ script.js
@@ -6,10 +6,13 @@ fetch(endpoint)
     .then(stuff => data.push(...stuff))
 console.log(data)
 
-// Search in data
-function findMatches(wordToMatch, data) {
+// Search between 2 city
+function findMatches(city1, city2, data) {
     return data.filter(place => {
-        const regex = new RegExp(wordToMatch, 'gi')
-        return place.Departure.match(regex) || place.Arrival.match(regex)
+        const regex1 = new RegExp(city1, 'gi')
+        const regex2 = new RegExp(city2, 'gi')
+
+        return (place.Departure.match(regex1) || place.Arrival.match(regex1))
+        && (place.Departure.match(regex2) || place.Arrival.match(regex2))
     })
 }
