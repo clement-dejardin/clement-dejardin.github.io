7388afe9f60be9bd6c447c7b443e6cf112a3b971
Clément <clem74500@live.fr>
2020-08-03 18:23:02 +0200

    New project: Meeting with Suzy

--- script.js
+++ script.js
@@ -1,167 +1,34 @@
-// Sorting function
-var sortBy = function (property) {
-    return function (x, y) {
-        return ((x[property] === y[property]) ? 0 : ((x[property] > y[property]) ? 1 : -1));
-    };
-};
 
-// Input list
-const from = document.querySelector('.from')
-const to = document.querySelector('.to')
-const helicopter = document.querySelector('.helicopter')
-const pax = document.querySelector('.pax')
-const luggage = document.querySelector('.luggage')
-const departureZone = document.querySelector('.departure-zone')
-const engine = document.querySelector('.enginge-type')
-const sport = document.querySelector('.sport')
-const arrivalZone = document.querySelector('.arrival-zone')
+var countDownDate = new Date("Sept 12, 2020 14:00:00").getTime();
 
-// Button
-const search = document.querySelector('.search')
-const advancedSearch = document.querySelector('.advanced-search')
+var x = setInterval(function () {
 
-// Get data
-endpoint = 'https://clement-dejardin.github.io/data.json'
-const data = []
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(stuff => data.push(...stuff))
+    var now = new Date().getTime();
 
-// Search From and To
-function seachFromTo(from, to ,data) {
-    return data.filter(route => {
-        const regex1 = new RegExp(from, 'gi')
-        const regex2 = new RegExp(to, 'gi')
-        return route.Departure.match(regex1) && route.Arrival.match(regex2)
-    })
-}
+    var distance = countDownDate - now;
 
-// Dispay the result
-function displayMatches() {
-    const matchArray = searchFromTo(from.value, to.value, data)
-    // Sort matchArray by price
-    matchArray.sort(sortBy('Single_Pilot_Official_Price'))
-    const html = matchArray.map(route => {
-        return `
-    <div class="row">
-        <div class="main">
-            <div>
-                <h1>Heli alp</h1>
-                <div>
-                    <i class="fas fa-fw fa-phone-alt"></i> <i class="fas fa-fw fa-envelope"></i>
-                </div>
-            </div>
-            <div>
-                <div class="icon-text">
-                    <div>
-                        <i class="fas fa-fw fa-helicopter"></i>
-                    </div>
-                    <div>
-                        <p> h130</p>
-                    </div>
-                </div>
-            </div>
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    if (days < 10) {
+        day = '0' + days
+    }
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    if (hours < 10) {
+        hours = '0' + hours
+    }
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    if (minutes < 10) {
+        minutes = '0' + minutes
+    }
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    if (seconds < 10) {
+        seconds = '0' + seconds
+    }
 
-            <div>
-                <div class="icon-text">
-                    <div>
-                        <i class="fas fa-fw fa-users"></i>
-                    </div>
-                    <div>
-                        <p> 5</p>
-                    </div>
-                </div>
-                <p class="warning">*Max capacity assuming single pilot operation</p>
-            </div>
+    document.getElementById("counter").innerHTML = days + " jours <br>" + hours + " : " +
+        minutes + " : " + seconds;
 
-            <table>
-                <tr>
-                    <th></th>
-                    <th>Official Price</th>
-                    <th>Luna Price</th>
-                </tr>
-                <tr>
-                    <td><i class="fas fa-fw fa-user-tie"></i>1 Pilot</td>
-                    <td>5 000€</td>
-                    <td>4 000€</td>
-                </tr>
-                <tr>
-                    <td><i class="fas fa-fw fa-user-tie"></i>2 Pilots</td>
-                    <td>5 000€</td>
-                    <td>4 000€</td>
-                </tr>
-            </table>
-        </div>
-
-        <div class="secondary">
-            <div>
-                <div class="icon-text">
-                    <div>
-                        <i class="fas fa-fw fa-cogs"></i>
-                    </div>
-                    <div>
-                        <p> Engine </p>
-                        <p>Twin</p>
-                    </div>
-                </div>
-                <div class="icon-text">
-                    <div>
-                        <i class="fas fa-fw fa-hospital-symbol"></i>
-                    </div>
-                    <div>
-                        <p> Departure Zone </p>
-                        <p>Prevessins</p>
-                    </div>
-                </div>
-                <div class="icon-text">
-                    <div>
-                        <i class="fas fa-fw fa-suitcase"></i>
-                    </div>
-                    <div>
-                        <p> Luggage </p>
-                        <p>5</p>
-                    </div>
-                </div>
-                <div class="icon-text">
-                    <div>
-                        <i class="fas fa-fw fa-hospital-symbol"></i>
-                    </div>
-                    <div>
-                        <p> Arrival Zone </p>
-                        <p>Airport</p>
-                    </div>
-                </div>
-                <div class="icon-text">
-                    <div>
-                        <i class="fas fa-fw fa-luggage-cart"></i>
-                    </div>
-                    <div>
-                        <p> Sport Equipment </p>
-                        <p>Yes</p>
-                    </div>
-                </div>
-                <div class="icon-text">
-                    <div>
-                        <i class="fas fa-fw fa-fw fa-stopwatch"></i>
-                    </div>
-                    <div>
-                        <p> Flight Time </p>
-                        <p>1h 00</p>
-                    </div>
-                </div>
-            </div>
-
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ullamcorper, libero a laoreet
-                dignissim, risus sem euismod metus, eu hendrerit erat orci nec lacus.
-            </p>
-            <div>
-                <img src="https://c.eu12.content.force.com/servlet/servlet.FileDownload?file=0151r000006oMiw">
-            </div>
-        </div>
-    </div>
-        `
-    }).join('')
-    document.querySelector('.content').innerHTML = html
-}
-
-// Run displayMatches when typing in inpu
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "I Love You";
+    }
+}, 1000);
