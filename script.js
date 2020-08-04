c5e0c16448d0a1cc36af6de99fbd3335fd17def2
Clément <clem74500@live.fr>
2020-08-04 13:09:01 +0200

    Temps ensemble

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
-
-var countDownDate = new Date("Sept 12, 2020 14:00:00").getTime();
-
+// Time to next rendez-vous
+var nextrdv = new Date("Sept 12, 2020 14:00:00").getTime();
 var x = setInterval(function () {
-
     var now = new Date().getTime();
-
-    var distance = countDownDate - now;
-
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var tonextrdv = nextrdv - now;
+    var days = Math.floor(tonextrdv / (1000 * 60 * 60 * 24));
     if (days < 10) {
-        day = '0' + days
+        days = '0' + days
     }
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var hours = Math.floor((tonextrdv % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     if (hours < 10) {
         hours = '0' + hours
     }
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var minutes = Math.floor((tonextrdv % (1000 * 60 * 60)) / (1000 * 60));
     if (minutes < 10) {
         minutes = '0' + minutes
     }
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var seconds = Math.floor((tonextrdv % (1000 * 60)) / 1000);
     if (seconds < 10) {
         seconds = '0' + seconds
     }
-
-    document.getElementById("counter").innerHTML = days + " jours <br>" + hours + " : " +
+    document.getElementById("nextrdv").innerHTML = days + " jours - " + hours + " : " +
         minutes + " : " + seconds;
-
-    if (distance < 0) {
+    if (tonextrdv < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "I Love You";
+        document.getElementById("nextrdv").innerHTML = "I Love You";
     }
+
+}, 1000);
+
+// Time together
+var bday = new Date("10/07/20").getTime();
+var z = setInterval(function () {
+    var bnow = new Date().getTime();
+    var tillbday = bday - bnow;
+    var bdays = Math.floor(tillbday / (1000 * 60 * 60 * 24));
+    if (bdays < 10) {
+        bdays = '0' + bdays
+    }
+    document.getElementById("together").innerHTML = bdays + " jours"
 }, 1000);
