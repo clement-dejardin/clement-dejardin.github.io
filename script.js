a8e83121621d7f1bd81770041b8940488f6a96ae
Clément Dejardin <clem74500@live.fr>
2020-10-15 17:46:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Time to next rendez-vous
-var nextrdv = new Date("Oct 19, 2020 18:00:00").getTime();
+var nextrdv = new Date("Oct 18, 2020 14:00:00").getTime();
 var x = setInterval(function () {
     var now = new Date().getTime();
     var tonextrdv = nextrdv - now;
